f59c2af7b2b33e916eab4496b553f9111979ffc9
Ali Abbas <aliabbas.299792@gmail.com>
2023-10-17 15:28:13 +0100

    Added comment, transformed to np.array

--- loading.py
+++ loading.py
@@ -5,12 +5,17 @@ def rot180(array: np.array) -> np.array:
   return np.rot90(np.rot90(array))
 
 def splitRoomWifiData(array: np.array) -> np.array:
+  # assumes each row is structured as such:
+  # [s1, s2, s3, s4, s5, s6, s7, roomNum] where
   grouped = {}
   for d in array:
     label = int(d[7])
     if not label in grouped:
       grouped[label] = []
     grouped[label].append(d[:7])
+
+  for g in grouped:
+    grouped[g] = np.array(grouped[g])
   return grouped
 
 def loadData():
