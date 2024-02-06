f82150ccd18e5e34fe5b49a79ce61f276daf1510
Mohamed Ali <mohamed.postbox@gmail.com>
2024-02-06 17:24:11 +0000

    feat(graph-experiment): made square graph and fixed printing

--- experiment.py
+++ experiment.py
@@ -1,51 +1,64 @@
 import time
 import curses
 
-from Bot import PositionControlBot, Bot
+from Bot import PowerControlBot, PositionControlBot, Bot
+
+global_stdscr = None
+
+def curse_print(text):
+    global_stdscr.clear()
+    global_stdscr.addstr(0, 0, text)
+    global_stdscr.refresh()
 
 
 def task1_control_loop(posBot: "PositionControlBot", letter: str):
     if letter == "w":
-        print("Moving forward")
+        curse_print("Moving forward")
         posBot.move_forward()
     elif letter == "a":
-        print("Turning left")
+        curse_print("Turning left")
         posBot.turn_left()
     elif letter == "s":
-        print("Moving backward")
+        curse_print("Moving backward")
         posBot.move_backward()
     elif letter == "d":
-        print("Turning right")
+        curse_print("Turning right")
         posBot.turn_right()
     elif letter == "y":
-        print("Moving in a square")
+        curse_print("Moving in a square")
         posBot.move_square()
     elif letter == "x":
-        print("Stopping")
+        curse_print("Stopping")
         posBot.stop()
     else:
-        print(f"Unknown command: {letter}")
+        curse_print(f"Unknown command: {letter}")
 
 
 class ControlProcedure:
     def __init__(self, control_loop_fn):
-        self.stdscr = curses.initscr()
         self.control_loop_fn = control_loop_fn
         curses.noecho()
 
     def start_procedure(self):
         while True:
-            inp = self.stdscr.getkey()
+            inp = global_stdscr.getkey()
             self.control_loop_fn(inp)
             time.sleep(0.1)
 
 
-if __name__ == "__main__":
-    bot = Bot()
-    posControlBot = PositionControlBot(bot)
+def main(stdscr):
+    global global_stdscr
+    global_stdscr = stdscr
 
+    bot = Bot()
+    posControlBot = PowerControlBot(bot, 50)
+    
     def control_loop_fn(inp: str):
         task1_control_loop(posControlBot, inp)
 
     control_proc = ControlProcedure(control_loop_fn)
     control_proc.start_procedure()
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
