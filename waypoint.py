0f8c367330bf6681f6d9d46d713303ea3f32fcc2
Mohamed Ali <mohamed.postbox@gmail.com>
2024-02-13 17:48:17 +0000

    fix(waypoint-navigation): correct angle to turn to go towards waypoints

--- waypoint.py
+++ waypoint.py
@@ -14,7 +14,11 @@ BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be 
 motorR = BP.PORT_B # right motor
 motorL = BP.PORT_C # left motor
 speed = 360   # range is -255 to 255, make lower if bot it too fast
-x, y, theta = 0, 0, 0
+x, y, theta = 0.0, 0.0, 0.0
+
+BP.set_motor_limits(motorR, 50)
+BP.set_motor_limits(motorL, 50)
+
 #Move Forward
 
 def fwd():
@@ -45,19 +49,23 @@ def stop():
         BP.set_motor_power(motorR, 0)
         BP.set_motor_power(motorL, 0)
 
+
 def navigateToWaypoint(wx, wy):
-    meter = 2082.5
+    meter = (833 / 4) * 10
     rotate = 1080
     global x
     global y
     global theta
     x_new, y_new = wx - x, wy - y
-    angle = math.degrees(math.atan(y_new/x_new)) + (270 - theta)
+    angle = math.degrees(math.atan2(y_new, x_new)) - theta
     dist = math.sqrt(x_new**2 + y_new**2)
     pos_r = BP.get_motor_encoder(motorR)
     pos_l = BP.get_motor_encoder(motorL)
-
-    motorTurnAmount = rotate / (360 / angle)
+#    if (angle > 180):
+#        angle -= 180
+#    elif (angle < 180):
+#        angle += 180
+    motorTurnAmount = rotate * (angle / 360)
     motorDriveAmount = meter * dist
 
     BP.set_motor_position(motorR, pos_r - motorTurnAmount)
@@ -67,7 +75,16 @@ def navigateToWaypoint(wx, wy):
     pos_l = BP.get_motor_encoder(motorL)
     BP.set_motor_position(motorR, pos_r + motorDriveAmount)
     BP.set_motor_position(motorL, pos_l + motorDriveAmount)
-    x, y, theta = wx, wy, angle
+    x, y = wx, wy
+    a = theta + angle
+#    if a > 180:
+#        a -= 180
+#    elif a < 180:
+#        a += 180
+    theta = a
+    print(x_new)
+    print(y_new)
+    print(theta)
     time.sleep(4)
 
 
@@ -75,7 +92,7 @@ try:
         while True:
                 inp_x = stdscr.getkey() 
                 inp_y = stdscr.getkey() 
-                navigateToWaypoint(inp_x, inp_y)
+                navigateToWaypoint(float(inp_x), float(inp_y))
 
                 time.sleep(0.01)
 except KeyboardInterrupt:
