9401c9283a39b37bdcd2d4e652ec9532316fc81f
Mohamed Ali <mohamed.postbox@gmail.com>
2024-02-14 21:14:25 +0000

    fix(waypoint-navigation): correct movement towards waypoints

--- waypoint.py
+++ waypoint.py
@@ -11,38 +11,13 @@ stdscr = curses.initscr()
 curses.noecho()
 
 BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
-motorR = BP.PORT_B # right motor
-motorL = BP.PORT_C # left motor
+motorR = BP.PORT_C # right motor
+motorL = BP.PORT_B # left motor
 speed = 360   # range is -255 to 255, make lower if bot it too fast
 x, y, theta = 0.0, 0.0, 0.0
 
 BP.set_motor_limits(motorR, 50)
 BP.set_motor_limits(motorL, 50)
-
-#Move Forward
-
-def fwd():
-        pos_r = BP.get_motor_encoder(motorR)
-        pos_l = BP.get_motor_encoder(motorL)
-        #BP.set_motor_power(motorR,speed)
-        #BP.set_motor_power(motorL,speed)
-        BP.set_motor_position(motorR, pos_r + speed)
-        BP.set_motor_position(motorL, pos_l + speed)
-def left():
-        pos_r = BP.get_motor_encoder(motorR)
-        pos_l = BP.get_motor_encoder(motorL)
-        #BP.set_motor_power(motorL, speed)
-        #BP.set_motor_power(motorR, -speed)
-        BP.set_motor_position(motorR, pos_r - 270)
-        BP.set_motor_position(motorL, pos_l + 270)
-        while not (encoder_reached(pos_r - 270, motorR) and encoder_reached(pos_l + 270, motorL)):
-               time.sleep(0.1)
-#Move Right
-def right():
-        pos_r = BP.get_motor_encoder(motorR)
-        pos_l = BP.get_motor_encoder(motorL)
-        BP.set_motor_position(motorR, pos_r + 270)
-        BP.set_motor_position(motorL, pos_l - 270)
 
 #Stop
 def stop():
@@ -65,34 +40,41 @@ def navigateToWaypoint(wx, wy):
 #        angle -= 180
 #    elif (angle < 180):
 #        angle += 180
-    motorTurnAmount = rotate * (angle / 360)
-    motorDriveAmount = meter * dist
 
-    BP.set_motor_position(motorR, pos_r - motorTurnAmount)
-    BP.set_motor_position(motorL, pos_l + motorTurnAmount)
+    motorDriveAmount = meter * dist
+    print("angle: " + str(angle))
+    if angle < -180:
+        angle += 360
+    elif angle > 180:
+        angle -= 360
+    motorTurnAmount = rotate * (angle / 360)
+    BP.set_motor_position(motorR, pos_r + motorTurnAmount)
+    BP.set_motor_position(motorL, pos_l - motorTurnAmount)
     time.sleep(2)
     pos_r = BP.get_motor_encoder(motorR)
     pos_l = BP.get_motor_encoder(motorL)
     BP.set_motor_position(motorR, pos_r + motorDriveAmount)
     BP.set_motor_position(motorL, pos_l + motorDriveAmount)
     x, y = wx, wy
-    a = theta + angle
-#    if a > 180:
-#        a -= 180
-#    elif a < 180:
-#        a += 180
-    theta = a
-    print(x_new)
-    print(y_new)
-    print(theta)
+    theta += angle
     time.sleep(4)
 
 
 try:
         while True:
-                inp_x = stdscr.getkey() 
-                inp_y = stdscr.getkey() 
-                navigateToWaypoint(float(inp_x), float(inp_y))
+                #inp_x = stdscr.getkey() 
+                #inp_y = stdscr.getkey()
+                stdscr.clear()
+                stdscr.addstr(0, 0, "Waypoint coordinate in format x,y")
+                stdscr.refresh()
+                curses.echo()
+                inp = stdscr.getstr(1,0).decode(encoding='utf-8')
+                curses.noecho()
+                x_str, y_str = map(str.strip, inp.split(','))
+                inp_x = float(x_str)
+                inp_y = float(y_str)         
+                print(f"moving to ({inp_x}, {inp_y})")  
+                navigateToWaypoint(inp_x, inp_y)
 
                 time.sleep(0.01)
 except KeyboardInterrupt:
