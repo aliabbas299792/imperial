1d7954b4c765d4eb31f570e7b5a0c9065d2c8c0b
Ali Abbas <aliabbas.299792@gmail.com>
2024-11-27 14:58:24 +0000

    test(70068-lcl): updated tests to check for topological order

--- scheduling_resource_allocation/common/common.py
+++ scheduling_resource_allocation/common/common.py
@@ -1,4 +1,19 @@
+from models.Schedule import Schedule
+from models.DirectedAcyclicGraph import DirectedAcyclicGraph
 from models.Node import Node
+
 
 def tardiness_cost_fn(C_j: int, node: Node):
     return max(0, C_j - node.due_date)
+
+
+def is_topologically_valid(schedule: Schedule, dag: DirectedAcyclicGraph) -> bool:
+    position = {node: idx for idx, node in enumerate(schedule)}
+
+    # check if all predecessors of every node in the schedule actually precedes that node
+    for node in schedule:
+        for predecessor in dag.reverse_adjacency_matrix.get(node, []):
+            if position[predecessor] > position[node]:
+                return False
+
+    return True
--- scheduling_resource_allocation/tests/LowestCostLastSchedulerTests.py
+++ scheduling_resource_allocation/tests/LowestCostLastSchedulerTests.py
@@ -2,6 +2,7 @@ import random
 import pytest
 
 from common.constants import CW_DAG_PATH
+from common.common import is_topologically_valid
 from core.LowestCostLassScheduler import LowestCostLastScheduler
 from models.TardySchedule import TardySchedule
 from models.DirectedAcyclicGraph import DirectedAcyclicGraph
@@ -40,11 +41,17 @@ def test_lcl_schedule_cost(dag: DirectedAcyclicGraph):
 
     scheduler = LowestCostLastScheduler(dag)
     schedule = scheduler.lcl()
+    assert is_topologically_valid(
+        schedule, dag
+    ), "LCL schedule is not topologically valid"
     found_schedule_cost = schedule.maximum_cost()
 
     costs = []
     for _ in range(10000):
         random_schedule = generate_random_tardy_topological_schedule(dag)
+        assert is_topologically_valid(
+            random_schedule, dag
+        ), "Randomly generated schedule is not topologically valid"
         costs.append(random_schedule.maximum_cost())
 
     assert found_schedule_cost <= min(costs), (
