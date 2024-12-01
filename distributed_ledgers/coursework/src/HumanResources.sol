55e8819e2f4d53b5f2afb3af20f72d53426a5674
Ali Abbas <aliabbas.299792@gmail.com>
2024-12-01 13:25:31 +0000

    feat(70017-setup): added setup code for contract

--- distributed_ledgers/coursework/src/HumanResources.sol
+++ distributed_ledgers/coursework/src/HumanResources.sol
@@ -10,5 +10,27 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 contract HumanResources is IHumanResources {
     using SafeERC20 for IERC20;
 
+    // Constants for contract addresses and configuration
+    address private constant WETH = 0x4200000000000000000000000000000000000006;
+    address private constant USDC = 0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85;
+
+    // External contracts
+    ISwapRouter private constant swapRouter =
+        ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+    AggregatorV3Interface private constant ethUsdFeed =
+        AggregatorV3Interface(0x13e3Ee699D1909E989722E753853AE30b17e08c5);
+
+    address public immutable hrManager;
+
+    constructor(address _hrManager) {
+        require(_hrManager != address(0), "Invalid HR manager address");
+        hrManager = _hrManager;
+    }
+
+    modifier onlyHrManager() {
+        if (msg.sender != hrManager) revert NotAuthorized();
+        _;
+    }
+
     receive() external payable {}
 }
